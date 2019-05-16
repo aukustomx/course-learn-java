4fae8448fd92b4739ed20f282a1b17ce8bcad4bd
Augusto Alonso de la Cruz Jimenez <augustoalonso.delacruz@bbva.com>
2019-05-16 16:37:09 -0500

    Clase que imprime del 1 al 10

--- lesson4/OneToTen.java
+++ lesson4/OneToTen.java
@@ -1,7 +1,10 @@
 class OneToTen {
+
     public static void main(String[] args){
-        for(int i=1; i<11; i++){
+        
+        for(int i = 1; i < 11; i++){
             System.out.println(i);
         }
     }
 }
+
