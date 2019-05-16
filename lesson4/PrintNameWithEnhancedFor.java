2407231cc8ce9a01d6ac7e2fed57e62bb2961b89
Augusto Alonso de la Cruz Jimenez <augustoalonso.delacruz@bbva.com>
2019-05-16 17:29:58 -0500

    Código ejemplo

---
+++ lesson4/PrintNameWithEnhancedFor.java
@@ -0,0 +1,12 @@
+class PrintNameWithEnhancedFor {
+    
+    public static void main(String[] args) {
+        
+        String[] names = {"Marco", "Claudia", "Abraham", "Roberto"};
+        
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
+
