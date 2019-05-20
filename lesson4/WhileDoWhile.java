2ea3606278e477463b17fa03594071fec9c42e3f
Augusto Alonso de la Cruz Jimenez <augustoalonso.delacruz@bbva.com>
2019-05-20 09:36:29 -0500

    ejemplos de switch, while y do-while

---
+++ lesson4/WhileDoWhile.java
@@ -0,0 +1,20 @@
+public class WhileDemo {
+    
+	public static void main(String[] args){
+        
+		//while loop
+		int count = 1;
+	        while (count < 11) {
+        	    System.out.println("Count is: " + count);
+	            count++;
+        	}
+
+		//do-while loop
+		int counter = 1;
+		do {
+			System.out.println("Counter is: " + counter);
+			counter++;
+		} while (counter < 11);
+    	}
+}
+
